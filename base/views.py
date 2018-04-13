ec62e1d26fa0c6901145df4f30f904f531d6427e
mixedmilk <2502799254@qq.com>
2018-04-13 17:39:31 +0800

    v3

--- base/views.py
+++ base/views.py
@@ -25,7 +25,7 @@ def login(request):
         user = authenticate(username=username, password=password)
         if user is not None:
             base_login(request, user)
-            return render(request, "welcome.html", {'msg': 'welcome to '})
+            return render(request, "welcome.html", {'msg': 'welcome to hahah'})
         else:
             print "no.."
     #     user = User.objects.all().filter(username=username)
