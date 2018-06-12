ca47816437304111902e9c6186e0169524d0d061
mixedmilk <2502799254@qq.com>
2018-06-12 19:31:30 +0800

    co

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,7 +1,15 @@
 from django.conf.urls import url
 from snippets import views
+from rest_framework.urlpatterns import format_suffix_patterns
+# urlpatterns = [
+#     url(r'^$', views.snippet_list),
+#     url(r'^(?P<pk>[0-9]+)/$', views.snippet_detail),
+# ]
+#
+# urlpatterns = format_suffix_patterns(urlpatterns)
 
 urlpatterns = [
-    url(r'^$', views.snippet_list),
-    url(r'^(?P<pk>[0-9]+)/$', views.snippet_detail),
+    url(r'^$',views.SnippetList.as_view()),
+    url(r'(?P<pk>[0-9]+)/$',views.SnippetDetail.as_view())
 ]
+urlpatterns = format_suffix_patterns(urlpatterns)
--- snippets/views.py
+++ snippets/views.py
@@ -14,7 +14,56 @@ from rest_framework.parsers import JSONParser
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 from rest_framework import permissions
+from rest_framework.views import APIView
+from django.http import Http404
 
+
+class SnippetList(APIView):
+
+    def get_object(self, pk):
+        try:
+            return Snippet.objects.get(pk=pk)
+        except Snippet.DoesNotExist:
+            raise Http404
+
+
+    def get(self, request, format=None):
+        snippets = Snippet.objects.all()
+        serializer = SnippetSerializer(snippets, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = SnippetSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class SnippetDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Snippet.objects.get(pk=pk)
+        except Snippet.DoesNotExist:
+            raise Http404
+
+
+    def get(self, request, format=None):
+        snippets = Snippet.objects.all()
+        serializer = SnippetSerializer(snippets, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = SnippetSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        snippet = self.get_object(pk)
+        snippet.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 '''
 @csrf_exempt
@@ -61,7 +110,7 @@ def snippet_detail(request, pk):
 '''
 
 @api_view(["GET", "POST"])
-def snippet_list(request):
+def snippet_list(request, format=None):
     if request.method == 'GET':
         snippets = Snippet.objects.all()
         serializer = SnippetSerializer(snippets, many=True)
@@ -79,7 +128,7 @@ def snippet_list(request):
 
 @api_view(['GET', 'PUT', 'DELETE'])
 @permission_classes((permissions.AllowAny,))
-def snippet_detail(request, pk):
+def snippet_detail(request, pk, format=None):
     """
     Retrieve, update or delete a code snippet.
     """
